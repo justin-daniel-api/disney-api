3c44df439b5d206bfba6d369dd4211ebd4e15676
Justin Berg <justin.berg1@verizon.com>
2025-03-04 15:57:11 -0600

    showing disney characters names

--- my-disney-app/src/app/components/disney-characters/disney-characters.component.ts
+++ my-disney-app/src/app/components/disney-characters/disney-characters.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+// import { MatCell, MatCellDef, MatHeaderCell, MatHeaderCellDef, MatRow, MatRowDef, MatTable, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-disney-characters',
-  imports: [],
+  imports: [CommonModule,
+    // MatTable,
+    // MatHeaderCell,
+    // MatHeaderCellDef,
+    // MatCell,
+    // MatCellDef,
+    // MatRow,
+    // MatRowDef
+  ],
   templateUrl: './disney-characters.component.html',
-  styleUrl: './disney-characters.component.css'
+  styleUrl: './disney-characters.component.css',
+  standalone: true,
 })
-export class DisneyCharactersComponent {
 
+export class DisneyCharactersComponent implements OnInit {
+  private http = inject(HttpClient);
+  characters: any[] = [];
+
+  ngOnInit(): void {
+    this.http.get('https://api.disneyapi.dev/character').subscribe((data: any) => {
+      this.characters = data.data;
+    });
+  }
 }
