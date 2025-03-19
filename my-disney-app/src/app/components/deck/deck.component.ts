93f07c51af485f21133e2396bef1309cdae0f57a
Justin Berg <justin.berg1@verizon.com>
2025-03-19 15:04:44 -0500

    created all components

---
+++ my-disney-app/src/app/components/deck/deck.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deck',
+  imports: [],
+  templateUrl: './deck.component.html',
+  styleUrl: './deck.component.css'
+})
+export class DeckComponent {
+
+}
