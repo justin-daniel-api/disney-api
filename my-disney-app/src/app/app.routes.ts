03076132c0cfa5749a93d4cb4e0a3904e55202b2
danny paiz <daniel.martinez.paiz@verizon.com>
2025-03-19 15:53:49 -0400

    added pagenotfound component, updated app.routes with wildcard fallback component to stay at the end of list

--- my-disney-app/src/app/app.routes.ts
+++ my-disney-app/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { DisneyCharactersComponent } from './components/disney-characters/disney-characters.component';
 import { NgModel } from '@angular/forms';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'characters',
         component: DisneyCharactersComponent
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
     }
 ];
