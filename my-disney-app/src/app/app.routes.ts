3b0096fe21db6c251faebc1bba8ba4dfe8d57517
Justin Berg <justin.berg1@verizon.com>
2025-03-19 15:17:15 -0500

    added imports to route page

--- my-disney-app/src/app/app.routes.ts
+++ my-disney-app/src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DisneyCharactersComponent } from './components/disney-characters/disney-characters.component';
 import { NgModel } from '@angular/forms';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { DeckComponent } from './components/deck/deck.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SingleCharacterComponent } from './components/single-character/single-character.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
 
 export const routes: Routes = [
     {
@@ -13,7 +18,7 @@ export const routes: Routes = [
         component: DisneyCharactersComponent
     },
     {
-        path: 'character/:id'
+        path: 'character/:id',
         component: SingleCharacterComponent
     },
     {
