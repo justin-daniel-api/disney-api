2a66e93c174b4abe1e24742317ab9305e12fb3e3
Justin Berg <justin.berg1@verizon.com>
2025-03-21 12:54:00 -0500

    functionality for adding to deck and viewing single character

--- my-disney-app/src/app/app.routes.ts
+++ my-disney-app/src/app/app.routes.ts
@@ -18,7 +18,7 @@ export const routes: Routes = [
         component: DisneyCharactersComponent
     },
     {
-        path: 'character/:id',
+        path: 'character/:name',
         component: SingleCharacterComponent
     },
     {
--- my-disney-app/src/app/components/disney-characters/disney-characters.component.ts
+++ my-disney-app/src/app/components/disney-characters/disney-characters.component.ts
@@ -1,44 +1,38 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-// import { MatCell, MatCellDef, MatHeaderCell, MatHeaderCellDef, MatRow, MatRowDef, MatTable, MatTableModule } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { CharacterService } from '../../services/character.service';
 
 @Component({
   selector: 'app-disney-characters',
-  imports: [CommonModule,
-    // MatTable,
-    // MatHeaderCell,
-    // MatHeaderCellDef,
-    // MatCell,
-    // MatCellDef,
-    // MatRow,
-    // MatRowDef
-  ],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './disney-characters.component.html',
   styleUrl: './disney-characters.component.css',
-  standalone: true,
 })
-
 export class DisneyCharactersComponent implements OnInit {
   private http = inject(HttpClient);
+  private router = inject(Router);
+  private characterService = inject(CharacterService);
+
   characters: any[] = [];
 
   ngOnInit(): void {
     this.getCharacters();
   }
 
-addToDeck(): any {
-  
-}
-
-viewDetails(): any {
-
-}
-
-
   getCharacters(): void {
     this.http.get('https://api.disneyapi.dev/character').subscribe((data: any) => {
       this.characters = data.data;
     });
+  }
+
+  addToDeck(character: any): void {
+    this.characterService.addToDeck(character, this.characters);
+  }
+
+  viewDetails(character: any): void {
+    this.router.navigate([`/character/${character.name}`]);
   }
 }
